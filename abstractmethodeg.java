c25aec4448d1e2ff55367253c0e7c421a1bdc7ec
ustutee <ustutee@gmail.com>
2026-02-03 13:16:37 +0530

    Add files via upload

---
+++ abstractmethodeg.java
@@ -0,0 +1,15 @@
+abstract class Animal {
+    abstract void sound();
+
+    void eat(){
+        System.out.println("this animal eats food.");
+    }
+}
+class Dog extends Animal {
+    void sound() {
+        System.out.println("dog barks");
+    }
+}
+public class abstractmethodeg {
+    
+}
