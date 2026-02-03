8b5f0430c0e007d8151ae6b42d398ff4af1c4a35
ustutee <ustutee@gmail.com>
2026-02-03 13:43:31 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,8 @@
+class calculator {
+    static int add(int a, int b) {
+        return a +b;
+    }
+     public static void main (String[] args){
+        System.out.println("Sum:" + calculator.add(10,20));
+     }
+}
--- callcons.java
+++ callcons.java
@@ -3,11 +3,17 @@ class Demo{
         this ("Default Constructor");
     }
     Demo(String message) {
-        System.out.println(message);
+        System.out.println(message);        
+    }
+    Demo(String message, int number){
+        System.out.println(message + " " + number);
+
     }
 }
 public class callcons {
    public static void main(String[] args) {
          new Demo();
+         new Demo("hello");
+         new Demo("hi",10);
    }   
 }
